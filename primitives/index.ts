31637d5893ee89af763e77066f734715ecaad148
Unto Death <git@untodeath.jp>
2023-05-10 18:29:19 -0500

    Export react-wrap-balancer Provider as Balancer.Provider

--- primitives/index.ts
+++ primitives/index.ts
@@ -1,4 +1,9 @@
-export { Balancer, Provider } from "react-wrap-balancer";
+import ReactWrapBalancer, { Provider } from "react-wrap-balancer";
 export { Toaster, toast } from "sonner";
 export * from "./Button";
 export * from "./Link";
+
+export const Balancer: typeof ReactWrapBalancer = Object.assign(
+  ReactWrapBalancer,
+  { Provider }
+);
